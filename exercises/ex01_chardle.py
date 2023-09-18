df7b6de75adf03268a01b8f5392619aa31656120
Nathan Day <daynr@unc.edu>
2023-09-17 22:29:15 -0400

    AGAIN!!!

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,7 +5,6 @@ __author__ = "730690615"
 # Prompting the Inputs
 word = input("Enter a 5-character word: ")
 char = input("Enter a single character: ")
-print(f"Searching for {char} in {word}")
 
 # Move the length check outside of the main code
 if len(word) != 5:
@@ -18,42 +17,44 @@ elif len(char) != 1:
 else:
     found = False
 
-    # Create a variable to count matching characters
-    count = 0
+print(f"Searching for {char} in {word}")
 
-    if word[0] == char:
-        print(f"Searching for {char} in {word}")
-        print(f"{char} found at index 0")
-        count += 1
-        found = True
-    if word[1] == char:
-        print(f"Searching for {char} in {word}")
-        print(f"{char} found at index 1")
-        count += 1
-        found = True
-    if word[2] == char:
-        print(f"Searching for {char} in {word}")
-        print(f"{char} found at index 2")
-        count += 1
-        found = True
-    if word[3] == char:
-        print(f"Searching for {char} in {word}")
-        print(f"{char} found at index 3")
-        count += 1 
-        found = True
-    if word[4] == char:
-        print(f"Searching for {char} in {word}")
-        print(f"{char} found at index 4")
-        count += 1
-        found = True
+# Create a variable to count matching characters
+count = 0
+
+if word[0] == char:
+    print(f"Searching for {char} in {word}")
+    print(f"{char} found at index 0")
+    count += 1
+    found = True
+if word[1] == char:
+    print(f"Searching for {char} in {word}")
+    print(f"{char} found at index 1")
+    count += 1
+    found = True
+if word[2] == char:
+    print(f"Searching for {char} in {word}")
+    print(f"{char} found at index 2")
+    count += 1
+    found = True
+if word[3] == char:
+    print(f"Searching for {char} in {word}")
+    print(f"{char} found at index 3")
+    count += 1 
+    found = True
+if word[4] == char:
+    print(f"Searching for {char} in {word}")
+    print(f"{char} found at index 4")
+    count += 1
+    found = True
 
     # Check the count to determine the result
 
-    if count == 1:
-        print(f"{count} instance of {char} found in {word}")
-    else:
-        print(f"{count} instances of {char} found in {word}")
+if count == 1:
+    print(f"{count} instance of {char} found in {word}")
+else:
+    print(f"{count} instances of {char} found in {word}")
 
     # Check the flag to determine the result
-    if not found:
-        print(f"No instances of {char} found in {word}")
+if not found:
+    print(f"No instances of {char} found in {word}")

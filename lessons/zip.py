a8248e9c96e55501d1587fe01ee841e8b1d09d8e
Nathan Day <daynr@unc.edu>
2023-10-27 19:22:33 -0400

    Commit #1 for Zippy

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,16 +1,19 @@
 """Combining two lists into a dictionary."""
+
 from typing import List, Dict
 
 __author__ = "730690615"
 
 
-def zip_lists(str_list: List[str], int_list: List[int]) -> Dict[str, int]:
-    """Check if the input lists have the same length and must not be empty."""
-    if len(str_list) != len(int_list) or len(str_list) == 0:
+def zip(str_list: List[str], int_list: List[int]) -> Dict[str, int]:
+    """Combine two lists into a dictionary."""
+    # Check if the input lists have different lengths or if they are empty
+    if len(str_list) != len(int_list) or not str_list or not int_list:
         return {}
     
-    # Use a dictionary to create the resulting Dict[str,int]:
-    result = {str_list[i]: int_list[i] for i in range(len(str_list))}
+    result = {}  # Initialize an empty dictionary
 
-    return result
+    for i in range(len(str_list)):
+        result[str_list[i]] = int_list[i]  # Create key-value pairs in the dictionary
     
+    return result
